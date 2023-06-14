8719531478c1a61fa4012da5a3f8d7b6e140c59e
priskilagrl <priskilalovikasari@gmail.com>
2023-06-15 03:00:49 +0700

    Auth Register & Profile

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -36,28 +39,27 @@ class AuthController extends Controller
     }
     public function register(Request $request)
     {
-        $credentials = [
-            'email' => $request->get('email'),
-            'password' => $request->get('password'),
-        ];
-        // return $credentials;
-        $token = Auth::attempt($credentials, true);
-        if (!$token) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized',
-            ], 401);
-        }
+        // Validate user input
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|min:8|',
+        ]);
 
-        $user = Auth::user();
+        // Create a new user
+        $user = User::create([
+            'uuid' => Str::uuid(),
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'remember_token' => Str::random(60), // Generate a random API token for the user
+        ]);
+
+        // Return a response with the created user and a success message
         return response()->json([
-            'status' => 'success',
-            'user' => $user,
-            'authorisation' => [
-                'token' => $token,
-                'type' => 'bearer',
-            ]
-        ], 200);
+            // 'user' => $user,
+            'message' => 'Registration successful!'
+        ], 201);
     }
     public function logout()
     {
@@ -65,5 +67,10 @@ class AuthController extends Controller
         return response()->json([
             'status' => 'success',
         ], 200);
+    }
+    public function profile()
+    {
+        $user = Auth::user();
+        return response()->json($user, 200);
     }
 }
